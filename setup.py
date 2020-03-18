49e4a6aa5724f950fadba08c020c923e507da921
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-03-18 16:12:18 +0100

    add python 3.4

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(keywords=['cython', 'extension', 'multiple', 'pyx'],
       install_requires=[
             'Cython'
       ],
-      python_requires='!=2.7.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+      python_requires='!=2.7.*,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
       )
