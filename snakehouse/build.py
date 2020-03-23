c35cd882eddea17332bed88f2c2ebe807f1ad17b
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-03-23 17:48:25 +0100

    fix build

--- snakehouse/build.py
+++ snakehouse/build.py
@@ -9,7 +9,10 @@ def build(extensions: tp.List[tp.Union[Multibuild, Extension]], *args, **kwargs)
     for multi_build in extensions:
         if isinstance(multi_build, Extension):
             returns.append(multi_build)
-        else:
+        elif isinstance(multi_build, Multibuild):
             multi_build.generate()
-            returns.append(multi_build.for_cythonize())
+        else:
+            raise ValueError('Invalid value in list, expected either an instance of Multibuild '
+                             'or an Extension')
+        returns.append(multi_build.for_cythonize())
     return cythonize(returns, *args, **kwargs)
