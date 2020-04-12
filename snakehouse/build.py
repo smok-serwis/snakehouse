d277f607576a4922aecc158d129ed5d8577edec6
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-04-12 23:03:46 +0200

    Fixes #1 (#6)
    * first take at this
    
    * test fixes
    
    * test fixes
    
    * fix tests
    
    * remove unnecessary imports

--- snakehouse/build.py
+++ snakehouse/build.py
@@ -1,3 +1,4 @@
+import logging
 import typing as tp
 from Cython.Build import cythonize
 from setuptools import Extension
@@ -5,16 +6,24 @@ from .multibuild import Multibuild
 
 MultiBuildType = tp.Union[Multibuild, Exception]
 
+logger = logging.getLogger(__name__)
+
 
 def build(extensions: tp.List[MultiBuildType], *args, **kwargs):
     returns = []
+    multi_builds = []
     for multi_build in extensions:
         if isinstance(multi_build, Extension):
             returns.append(multi_build)
         elif isinstance(multi_build, Multibuild):
             multi_build.generate()
+            multi_builds.append(multi_build)
             returns.append(multi_build.for_cythonize())
         else:
             raise ValueError('Invalid value in list, expected either an instance of Multibuild '
                              'or an Extension')
-    return cythonize(returns, *args, **kwargs)
+    values = cythonize(returns, *args, **kwargs)
+    for multi_build in multi_builds:
+        multi_build.do_after_cython()
+        logger.warning(multi_build.module_name_to_loader_function)
+    return values
