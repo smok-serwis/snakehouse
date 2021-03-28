435712b377b8a5d81018c071c9670ea4f3f6750a
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2021-03-28 20:27:47 +0200

    v1.4

--- snakehouse/__init__.py
+++ snakehouse/__init__.py
@@ -3,4 +3,4 @@ from .multibuild import Multibuild
 from .faster_builds import monkey_patch_parallel_compilation
 from .requirements import read_requirements_txt, find_c, find_pyx_and_c, find_pyx
 
-__version__ = '1.4a1'
+__version__ = '1.4'
